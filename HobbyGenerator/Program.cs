c5403576e0490f354dfca38ed2381053021853d8
CsLy0 <marius.sundt.skullestad@gmail.com>
2025-05-13 13:50:21 +0200

    Add project files.

---
+++ HobbyGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿Random rand = new Random();
+var randomNumber = rand.Next(0, 5);
+var randomName = rand.Next();
+string[] names = { "Marius", "Ole", "Michal", "Peder", "Vårin" };
+string randomNameString = names[rand.Next(names.Length)];
+
+if (randomNumber == 0)
+{
+    Console.WriteLine($"{randomNameString} liker twintip.");
+}
+else if (randomNumber == 1)
+{
+    Console.WriteLine($"{randomNameString} liker gaming.");
+}
+else if (randomNumber == 2)
+{
+    Console.WriteLine($"{randomNameString} liker tv-serier.");
+}
+else if (randomNumber == 3)
+{
+    Console.WriteLine($"{randomNameString} liker musikk.");
+}
+else
+{
+    Console.WriteLine($"{randomNameString} liker filmer."); ;
+}
